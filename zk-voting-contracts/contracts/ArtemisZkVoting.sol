e4822a97debc0efb7e6118dcd07d0e578a9dedba
auralshin <anshulspartan141@gmail.com>
2023-10-22 17:04:15 +0530

    changes: - updated contract with time baed proposals - quorum - added pause and unpause ability - auto-generation for votes added

--- zk-voting-contracts/contracts/ArtemisZkVoting.sol
+++ zk-voting-contracts/contracts/ArtemisZkVoting.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "./Verifier.sol";
 
-contract ArtemisDAOVoting is Ownable, Groth16Verifier {
+contract ArtemisDAOVoting is Ownable, Groth16Verifier, Pausable {
     Groth16Verifier public verifier;
 
     struct Proposal {
@@ -13,13 +14,23 @@ contract ArtemisDAOVoting is Ownable, Groth16Verifier {
         string proposalDescription;
         mapping(bool => uint256) votes; // true: support, false: against
         mapping(bytes32 => bool) voted; // leaf: hasVoted
+        uint256 startTime;
+        uint256 endTime;
+        uint256 quorum; // Expressed as a percentage
+        bool hasEnded;
     }
 
     mapping(uint256 => Proposal) public proposals;
+    uint256 public proposalCount;
 
     // Events
     event ProposalAdded(uint256 proposalId, string description);
     event VoteReceived(uint256 proposalId, bool support);
+    event ProposalEnded(
+        uint256 proposalId,
+        bool achievedQuorum,
+        bool supported
+    );
 
     // Custom errors
     error InvalidVote();
@@ -27,21 +38,24 @@ contract ArtemisDAOVoting is Ownable, Groth16Verifier {
     error InvalidZkSnarkProof();
     error InvalidMerkleProof();
     error InvalidProposal();
+    error VotingEnded();
+    error NotYetStarted();
 
     constructor(address initialOwner) Ownable(initialOwner) {}
 
     function addProposal(
-        uint256 proposalId,
         string memory description,
-        bytes32 _merkleRoot
+        bytes32 _merkleRoot,
+        uint256 duration,
+        uint256 _quorum
     ) external onlyOwner {
-        require(
-            proposals[proposalId].merkleRoot == 0,
-            "Proposal already exists."
-        );
-        proposals[proposalId].merkleRoot = _merkleRoot;
-        proposals[proposalId].proposalDescription = description;
-        emit ProposalAdded(proposalId, description);
+        proposalCount += 1;
+        proposals[proposalCount].merkleRoot = _merkleRoot;
+        proposals[proposalCount].proposalDescription = description;
+        proposals[proposalCount].startTime = block.timestamp;
+        proposals[proposalCount].endTime = block.timestamp + duration;
+        proposals[proposalCount].quorum = _quorum;
+        emit ProposalAdded(proposalCount, description);
     }
 
     function voteForProposal(
@@ -53,14 +67,17 @@ contract ArtemisDAOVoting is Ownable, Groth16Verifier {
         uint[2] calldata _pubSignals,
         bytes32[] calldata merkleProof,
         bytes32 leaf
-    ) external {
+    ) external whenNotPaused {
         Proposal storage proposal = proposals[proposalId];
-        if (proposal.merkleRoot == 0) {
-            revert InvalidProposal();
-        }
-        if (proposal.voted[leaf]) {
-            revert AlreadyVoted();
-        }
+        require(proposal.merkleRoot != 0, "Invalid proposal.");
+        require(!proposal.hasEnded, "Voting has ended for this proposal.");
+        require(
+            block.timestamp >= proposal.startTime,
+            "Voting not yet started."
+        );
+        require(block.timestamp <= proposal.endTime, "Voting time has ended.");
+        require(!proposal.voted[leaf], "Already voted.");
+
         if (!verifier.verifyProof(_pA, _pB, _pC, _pubSignals)) {
             revert InvalidZkSnarkProof();
         }
@@ -72,15 +89,26 @@ contract ArtemisDAOVoting is Ownable, Groth16Verifier {
         emit VoteReceived(proposalId, support);
     }
 
-    function totalVotesForProposal(
-        uint256 proposalId,
-        bool support
-    ) external view returns (uint256) {
+    function endVoting(uint256 proposalId) external {
         Proposal storage proposal = proposals[proposalId];
-        if (proposal.merkleRoot == 0) {
-            revert InvalidProposal();
-        }
-        return proposal.votes[support];
+        require(
+            block.timestamp > proposal.endTime,
+            "Voting period is not yet over."
+        );
+        require(
+            !proposal.hasEnded,
+            "Voting has already ended for this proposal."
+        );
+
+        proposal.hasEnded = true;
+
+        uint256 totalVotes = proposal.votes[true] + proposal.votes[false];
+        bool achievedQuorum = (totalVotes * 100) /
+            (proposal.votes[true] + proposal.votes[false] + totalVotes) >=
+            proposal.quorum;
+        bool isSupported = proposal.votes[true] > proposal.votes[false];
+
+        emit ProposalEnded(proposalId, achievedQuorum, isSupported);
     }
 
     function verifyMerkleProof(
@@ -89,6 +117,14 @@ contract ArtemisDAOVoting is Ownable, Groth16Verifier {
         bytes32 leaf
     ) internal view returns (bool) {
         return MerkleProof.verify(proof, proposal.merkleRoot, leaf);
+    }
+
+    function pauseVoting() external onlyOwner {
+        _pause();
+    }
+
+    function resumeVoting() external onlyOwner {
+        _unpause();
     }
 
     function transferContractOwnership(address newOwner) external onlyOwner {
