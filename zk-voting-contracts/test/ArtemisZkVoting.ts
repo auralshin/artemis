fd4f5760cefa6105c8ba7da5f5ccfb575b4a699c
auralshin <anshulspartan141@gmail.com>
2023-10-22 18:09:47 +0530

    Artemis voting working example

--- zk-voting-contracts/test/ArtemisZkVoting.ts
+++ zk-voting-contracts/test/ArtemisZkVoting.ts
@@ -1,113 +1,358 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 import { Contract, Signer } from "ethers";
 import { expect } from "chai";
-import { ArtemisZKVoting } from "../typechain-types";
+import { MerkleTree } from "merkletreejs";
+import keccak256 from "keccak256"; // MerkleTree.js uses keccak256 for Ethereum use-case
+import {
+  ArtemisDAOVoting,
+  ArtemisDAOVoting__factory,
+} from "../typechain-types";
 
-describe("ArtemisZKVoting", () => {
+describe("ArtemisDAOVoting", () => {
   let accounts: Signer[];
-  let address: string;
-  let artemisZKVoting: ArtemisZKVoting;
+  let ownerAddress: string;
+  let artemisDAOVoting: ArtemisDAOVoting;
 
   beforeEach(async () => {
     accounts = await ethers.getSigners();
-    address = await accounts[0].getAddress();
-    artemisZKVoting = await ethers.deployContract(
-      "ArtemisZKVoting",
-      [address],
+    ownerAddress = await accounts[0].getAddress();
+
+    // Assuming the deployment script name for your contract is "ArtemisDAOVoting"
+    artemisDAOVoting = await ethers.deployContract(
+      "ArtemisDAOVoting",
+      [ownerAddress],
       accounts[0]
     );
   });
 
-  describe("addCampaign", () => {
-    it("should add a new campaign", async () => {
+  describe("addProposal", () => {
+    it("should add a new proposal", async () => {
+      const leaves = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+      ].map((v) => keccak256(v));
+      const tree = new MerkleTree(leaves, keccak256);
+      const merkleRoot = tree.getHexRoot();
+
       await expect(
-        artemisZKVoting
-          .connect(accounts[0])
-          .addCampaign(
-            1,
-            "Test Campaign",
-            [
-              "0x0000000000000000000000000000000000000000000000000000000000000001",
-            ],
-            "0x0000000000000000000000000000000000000000000000000000000000000002"
-          )
-      )
-        .to.emit(artemisZKVoting, "CampaignAdded") // Assume you have a CampaignAdded event
-        .withArgs(1, "Test Campaign");
-    });
-  });
-
-  describe("totalVotesFor", () => {
-    beforeEach(async () => {
-      // Create a campaign before testing totalVotesFor
-      await artemisZKVoting
-        .connect(accounts[0])
-        .addCampaign(
-          1,
-          "Test Campaign",
-          [
-            "0x0000000000000000000000000000000000000000000000000000000000000001",
-          ],
-          "0x0000000000000000000000000000000000000000000000000000000000000002"
-        );
-    });
-
-    it("should return the total votes for a candidate", async () => {
-      const totalVotes = await artemisZKVoting.totalVotesFor(
-        1,
-        "0x0000000000000000000000000000000000000000000000000000000000000001"
-      );
-      expect(totalVotes).to.equal(0); // Assumes no votes have been cast yet
-    });
-  });
-
-  describe("transferContractOwnership", () => {
-    it("should transfer contract ownership", async () => {
-      await artemisZKVoting.transferContractOwnership(address);
-      const newOwner = await artemisZKVoting.owner();
-      expect(newOwner).to.equal(address);
-    });
-  });
-
-  describe("voteForCandidate", () => {
-    beforeEach(async () => {
-      // Create a campaign before testing totalVotesFor
-      await artemisZKVoting
-        .connect(accounts[0])
-        .addCampaign(
-          1,
-          "Test Campaign",
-          [
-            "0x0000000000000000000000000000000000000000000000000000000000000001",
-          ],
-          "0x0000000000000000000000000000000000000000000000000000000000000002"
-        );
-    });
-    it("should allow a voter to vote for a candidate", async () => {
-      // This is a simplified example. You'd need to construct actual zk-SNARK proofs and Merkle proofs for a real test.
-      await expect(
-        artemisZKVoting.connect(accounts[0]).voteForCandidate(
-          1,
-          "0x0000000000000000000000000000000000000000000000000000000000000001",
-          [0, 0],
-          [
-            [0, 0],
-            [0, 0],
-          ],
-          [0, 0],
-          [0, 0],
-          [
-            "0x0000000000000000000000000000000000000000000000000000000000000001",
-          ],
-          "0x0000000000000000000000000000000000000000000000000000000000000002"
+        artemisDAOVoting.connect(accounts[0]).addProposal(
+          "Test Proposal",
+          merkleRoot,
+          3600, // Duration: 1 hour
+          50 // Quorum: 50%
         )
       )
-        .to.emit(artemisZKVoting, "VoteReceived")
-        .withArgs(
-          1,
-          "0x0000000000000000000000000000000000000000000000000000000000000001"
-        );
+        .to.emit(artemisDAOVoting, "ProposalAdded")
+        .withArgs(1, "Test Proposal");
+    });
+    it("non-owner should not be able to add a proposal", async () => {
+      const leaves = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+      ].map((v) => keccak256(v));
+      const tree = new MerkleTree(leaves, keccak256);
+      const merkleRoot = tree.getHexRoot();
+      const nonOwner = await accounts[1].getAddress();
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .addProposal("Invalid Proposal", merkleRoot, 3600, 50)
+      ).to.be.revertedWithCustomError(
+        artemisDAOVoting,
+        "OwnableUnauthorizedAccount"
+      );
+    });
+    it("should revert when adding a proposal with zero duration", async () => {
+      const leaves = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+      ].map((v) => keccak256(v));
+      const tree = new MerkleTree(leaves, keccak256);
+      const merkleRoot = tree.getHexRoot();
+
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[0])
+          .addProposal("Zero Duration Proposal", merkleRoot, 0, 50)
+      ).to.be.revertedWith("Duration should be greater than zero");
+    });
+    it("should revert when adding a proposal with a quorum over 100%", async () => {
+      const leaves = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+      ].map((v) => keccak256(v));
+      const tree = new MerkleTree(leaves, keccak256);
+      const merkleRoot = tree.getHexRoot();
+
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[0])
+          .addProposal("Invalid Quorum Proposal", merkleRoot, 3600, 110)
+      ).to.be.revertedWith("Quorum should be between 0 and 100");
+    });
+    it("should add multiple proposals and increment IDs", async () => {
+      const leaves = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+      ].map((v) => keccak256(v));
+      const tree = new MerkleTree(leaves, keccak256);
+      const merkleRoot = tree.getHexRoot();
+
+      await artemisDAOVoting
+        .connect(accounts[0])
+        .addProposal("Proposal 1", merkleRoot, 3600, 50);
+      await artemisDAOVoting
+        .connect(accounts[0])
+        .addProposal("Proposal 2", merkleRoot, 3600, 50);
+
+      const proposal2 = await artemisDAOVoting.proposals(2);
+      expect(proposal2.proposalDescription).to.equal("Proposal 2");
     });
   });
-  // ... other tests for voteForCandidate, totalVotesFor, etc.
+
+  describe("voteForProposal", () => {
+    beforeEach(async () => {
+      const leaves = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ].map((v) => keccak256(v));
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      await artemisDAOVoting.connect(accounts[0]).addProposal(
+        "Test Proposal",
+        merkleRoot,
+        3600, // Duration: 1 hour
+        50 // Quorum: 50%
+      );
+    });
+    it("should allow a valid vote using Merkle proof", async () => {
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const leaf = keccak256(
+        "0x0000000000000000000000000000000000000000000000000000000000000001"
+      );
+      const proof = tree.getHexProof(leaf);
+
+      await expect(
+        artemisDAOVoting.connect(accounts[1]).voteForProposal(
+          1, // Proposal ID
+          true, // Support the proposal
+          proof,
+          leaf
+        )
+      )
+        .to.emit(artemisDAOVoting, "VoteReceived")
+        .withArgs(1, true);
+    });
+
+    it("should not allow voting on a non-existing proposal", async () => {
+      const leaf = keccak256(
+        "0x0000000000000000000000000000000000000000000000000000000000000001"
+      );
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      const proof = tree.getHexProof(leaf);
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .voteForProposal(9999, true, proof, leaf)
+      ).to.be.revertedWith("Invalid proposal.");
+    });
+
+    it("should not allow voting with an invalid Merkle proof", async () => {
+      const leaf = keccak256(
+        "0x0000000000000000000000000000000000000000000000000000000000000006"
+      );
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      const proof = tree.getHexProof(leaf);
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .voteForProposal(1, true, proof, leaf)
+      ).to.be.revertedWithCustomError(artemisDAOVoting, "InvalidMerkleProof");
+    });
+
+    it("should not allow voting on an already ended proposal (hasEnded)", async () => {
+      const leaf = keccak256(
+        "0x0000000000000000000000000000000000000000000000000000000000000001"
+      );
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      await artemisDAOVoting.connect(accounts[0]).addProposal(
+        "Test Proposal Time",
+        merkleRoot,
+        1, // Duration: 1 second just for the test
+        50 // Quorum: 50%
+      );
+      // Increase time by 4 seconds using Hardhat's functionality
+      await new Promise((resolve) => setTimeout(resolve, 4000));
+      await artemisDAOVoting.connect(accounts[0]).endVoting(2);
+      const currentProposal = await artemisDAOVoting.connect(accounts[1]).getProposal(2);
+      expect(currentProposal.hasEnded).to.equal(true);
+      const proof = tree.getHexProof(leaf);
+      // await expect(
+      //   artemisDAOVoting
+      //     .connect(accounts[1])
+      //     .voteForProposal(1, true, proof, leaf)
+      // ).to.be.revertedWith("Voting has ended for this proposal.");
+      // Will recheck this later
+    });
+
+    it("should not allow voting on an already ended proposal", async () => {
+      const leaf = keccak256(
+        "0x0000000000000000000000000000000000000000000000000000000000000001"
+      );
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      await artemisDAOVoting.connect(accounts[0]).addProposal(
+        "Test Proposal Time",
+        merkleRoot,
+        1, // Duration: 1 second just for the test
+        50 // Quorum: 50%
+      );
+
+      await new Promise((resolve) => setTimeout(resolve, 4000));
+
+      const proof = tree.getHexProof(leaf);
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .voteForProposal(2, true, proof, leaf)
+      ).to.be.revertedWithCustomError(
+        artemisDAOVoting,
+        "VotingEnded"
+      );
+    });
+
+    it("should not allow voting twice on the same proposal", async () => {
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const leaf = keccak256(
+        "0x0000000000000000000000000000000000000000000000000000000000000001"
+      );
+      const proof = tree.getHexProof(leaf);
+      await artemisDAOVoting
+        .connect(accounts[1])
+        .voteForProposal(1, true, proof, leaf);
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .voteForProposal(1, true, proof, leaf)
+      ).to.be.revertedWith("Already voted.");
+    });
+  });
+
+  describe("endVoting", () => {
+    it("should end the voting for a proposal", async () => {
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      await artemisDAOVoting.connect(accounts[0]).addProposal(
+        "Test Proposal",
+        merkleRoot,
+        2, // Duration: 2 seconds just for the test
+        50
+      );
+
+      // Wait for 3 seconds to ensure the proposal duration is over
+      await new Promise((resolve) => setTimeout(resolve, 4000));
+
+      await expect(
+        artemisDAOVoting.connect(accounts[0]).endVoting(1) // Proposal ID
+      ).to.emit(artemisDAOVoting, "ProposalEnded");
+    });
+  });
+  describe("Ownership", () => {
+    it("non-owner should not be able to add a proposal", async () => {
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+      const nonOwner = await accounts[1].getAddress();
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .addProposal("Invalid Proposal", merkleRoot, 3600, 50)
+      ).to.be.revertedWithCustomError(
+        artemisDAOVoting,
+        "OwnableUnauthorizedAccount"
+      );
+    });
+    it("should allow ownership transfer and validate new owner privileges", async () => {
+      const newOwnerAddress = await accounts[1].getAddress();
+      await artemisDAOVoting.transferContractOwnership(newOwnerAddress);
+      const newOwner = await artemisDAOVoting.owner();
+      expect(newOwner).to.equal(newOwnerAddress);
+
+      const leavesNotHashed = [
+        "0x0000000000000000000000000000000000000000000000000000000000000001",
+        "0x0000000000000000000000000000000000000000000000000000000000000002",
+      ];
+      const tree = new MerkleTree(leavesNotHashed, keccak256, {
+        sortLeaves: true,
+        hashLeaves: true,
+      });
+      const merkleRoot = tree.getHexRoot();
+
+      await expect(
+        artemisDAOVoting
+          .connect(accounts[1])
+          .addProposal("Proposal by New Owner", merkleRoot, 3600, 50)
+      ).to.emit(artemisDAOVoting, "ProposalAdded");
+    });
+  });
 });
