cf9ab6eaa775d07339d675cce5d0220dcee84ea3
auralshin <anshulspartan141@gmail.com>
2023-12-28 16:01:52 +0530

    updated changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "hardhat-gas-reporter";
 const config: HardhatUserConfig = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    currency: 'USD',
+    gasPrice: 21,
+    enabled: true,
+  },
 };
 
 export default config;
